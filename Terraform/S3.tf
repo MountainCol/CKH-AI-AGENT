23e8833400185ab1cc5b4b54511c391b938a23ad
MountainCol <156825484+MountainCol@users.noreply.github.com>
2025-08-08 15:53:44 +0100

    change:capitals to lowercase

--- Terraform/S3.tf
+++ Terraform/S3.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "AI-Agent-Chatbot" {
-    bucket = "AI-Agent-Chatbot-${random_string.random_suffix.result}"
+resource "aws_s3_bucket" "ai-agent-chatbot" {
+    bucket = "ai-agent-chatbot-${random_string.random_suffix.result}"
     force_destroy = true
 } 
 
-resource "aws_s3_bucket_versioning" "AI-Agent-Chatbot" {
-  bucket = aws_s3_bucket.AI-Agent-Chatbot.id
+resource "aws_s3_bucket_versioning" "ai-agent-chatbot" {
+  bucket = aws_s3_bucket.ai-agent-chatbot.id
   versioning_configuration {
     status = "Enabled"
-  }
+  }     
 }
