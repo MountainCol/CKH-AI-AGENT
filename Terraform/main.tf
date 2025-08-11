0797cc0a0c2cac63214124cf4f0b9111ad61cbb0
MountainCol <156825484+MountainCol@users.noreply.github.com>
2025-08-11 15:33:59 +0100

    add:providers for terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -8,7 +8,29 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.5"
     }
+    opensearch = {
+      source  = "opensearch-project/opensearch"
+      version = "2.2.0"
+    }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = ">= 1.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.6"
+    }
   }
+}
+
+# AWS Provider configuration
+provider "aws" {
+  region = "eu-west-1"  # Change to your preferred region
+}
+
+# OpenSearch provider configuration - this will be configured by the Bedrock module
+provider "opensearch" {
+  url = "http://localhost:9200"  # Placeholder - will be overridden by module
 }
 
 resource "random_string" "random_suffix" {
